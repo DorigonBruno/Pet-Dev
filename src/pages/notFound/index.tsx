1f997f01fddd6ccdbd4adefcbc18a28cf2d82926
Bruno Dorigon <brunodorigon@outlook.com>
2025-03-27 13:27:10 -0300

    build page notFound

--- src/pages/notFound/index.tsx
+++ src/pages/notFound/index.tsx
@@ -2,12 +2,16 @@ import { Link } from "react-router";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404</h1>
-      <p>Página não Encontrada</p>
+    <main className="w-full h-screen max-w-7xl m-auto mt-10">
+      <div className="flex flex-col justify-center gap-3 items-center font-usually">
+        <h1 className="text-xl md:text-3xl font-bold">404</h1>
+        <p className="text-slate-700 font-medium">Página não Encontrada</p>
 
-      <Link to={"/"}>Acessar Produtos</Link>
-    </div>
+        <Link to={"/"} className="underline font-medium">
+          Acessar Produtos
+        </Link>
+      </div>
+    </main>
   );
 };
 
